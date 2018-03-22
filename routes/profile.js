29fb9774240570dd455bb9a6f3ab302f47e97100
Vihanga Paranahewa <31098256+VihangaParanahewa@users.noreply.github.com>
2018-03-22 16:43:59 +0530

    new frontend

---
+++ routes/profile.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+var multer = require('multer');
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + '.jpg')
+    }
+});
+
+var upload = multer({ storage: storage }).single('profileImage');
+
+router.post('/', function (req, res) {
+    upload(req, res, function (err) {
+        if (err) {
+            console.log(err);
+        }
+        //console.log(req.files.file);
+        //console.log(res.filename);
+        res.json({
+            success: true,
+            message: 'Image uploaded!'
+        });
+
+        // Everything went fine
+    })
+});
+
+
+module.exports = router;
