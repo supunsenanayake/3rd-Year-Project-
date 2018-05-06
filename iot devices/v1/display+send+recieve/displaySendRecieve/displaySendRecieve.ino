932158fc035df0892b8a5e92654a67841a17ac30
kavindasilva <kss.kavinda@gmail.com>
2018-05-06 13:56:34 +0530

    iot code reviewing and commenting inserted

--- iot devices/v1/display+send+recieve/displaySendRecieve/displaySendRecieve.ino
+++ iot devices/v1/display+send+recieve/displaySendRecieve/displaySendRecieve.ino
@@ -8,11 +8,12 @@
   || #
 */
 #include <Wire.h>
-#include <LiquidCrystal_I2C.h>
+#include <LiquidCrystal_I2C.h> //lcd 
 #define BACKLIGHT_PIN     13
 
 
-
+// Set the pins on the I2C chip used for LCD connections:
+//addr, en,rw,rs,d4,d5,d6,d7,bl,blpol
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE); // Set the LCD I2C address
 
 
@@ -36,121 +37,125 @@ Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins , colPins, ROWS, COL
 
 int data_count = 0;
 char Data[32] = "";
+
+//setup init method -- MAIN
 void setup() {
-  lcd.begin(16, 2);
-  lcd.backlight();
-  Serial.begin(9600);
-  Serial2.begin(57600);
-  Serial3.begin(9600);
+	lcd.begin(16, 2);  //initialize the lcd for 16 chars 2 lines, turn on backlight
+	lcd.backlight();
+	Serial.begin(9600); //baud rate
+	Serial2.begin(57600);
+	Serial3.begin(9600);
 }
 
+//loop method -- MAIN
 void loop() {
- /* if (Serial2.available()) {
-    char x = Serial2.read();
-    delay(2000);
-    Serial.print("t");
-    Serial.print(x);
-    lcd.setCursor(0, 0);
-    lcd.print(x);
-   // Serial.print("/n"); 
+	/* if (Serial2.available()) {
+		char x = Serial2.read();
+		delay(2000);
+		Serial.print("t");
+		Serial.print(x);
+		lcd.setCursor(0, 0);
+		lcd.print(x);
+		// Serial.print("/n"); 
     
+	}*/
+
+	int  n=0;
+	while(Serial2.available()) //get input from Serial2
+	{
     
-  }*/
+		char x = Serial2.read();
+		delay(2);
+		//Serial.print("t");
+		if(isAlphaNumeric(x)||(x==' ')){
+			Serial.print(x);
+			lcd.setCursor(n, 0);
+			n++;
+			lcd.print(x);
+		} 
+	}  
+	
+	char customKey = customKeypad.getKey();
+	if (customKey) {
+		//delete char at a time
+		if (customKey == 'B') {
+			Serial.println("delete");
+			Serial.print("A datacount=");
+			Serial.println(data_count);
+			data_count = data_count - 1;
+			
+			if (data_count < 16) {
+				lcd.setCursor(data_count, 0);
+				lcd.print(" ");
+				Data[data_count] = '\0';
+			}
+			else {
+				lcd.setCursor(data_count - 16, 1);
+				lcd.print(" ");
+				Data[data_count] = '\0';
+			}
+		}
+		
+		else if (customKey == 'A') {
+			send();
+			data_count = 0;
+			lcd.clear();
+			lcd.setCursor(0, 0);
+			lcd.print("Massage Send...");
+			delay(1000);
+			lcd.clear();
+		}
+		
+		else {
+			if (data_count < 32) {
+				Serial.println(customKey);
+				
+				if (data_count < 16) {
+					lcd.setCursor(data_count, 0);
+					Data[data_count] = customKey;
+				}
+				else {
+					lcd.setCursor(data_count - 16, 1);
+					Data[data_count] = customKey;
+				}
+				lcd.print(customKey);
+				data_count++;
+				// delay(1000);
+				// lcd.clear();
+				// delay(80);
+			}
+			else {
+				lcd.clear();
+				lcd.setCursor(0, 0);
+				lcd.print("Sorry massage ");
+				lcd.setCursor(0, 1);
+				lcd.print("too long..");
+				Serial.print("B & datacount=");
+				Serial.println(data_count);
+				int temp=data_count;//-------------
+				Data[data_count] = '\n';
+				//data_count--;
+				// Serial.print("B datacount=");
+				//Serial.println(data_count);
+				delay(1000);
+				lcd.clear();
 
-int  n=0;
-while(Serial2.available())
-{
-    
-    char x = Serial2.read();
-    delay(2);
-    //Serial.print("t");
-    if(isAlphaNumeric(x)||(x==' ')){
-      Serial.print(x);
-      lcd.setCursor(n, 0);
-    n++;
-    lcd.print(x);
-      } 
-}  
-  char customKey = customKeypad.getKey();
-  if (customKey) {
-    //delete char at a time
-    if (customKey == 'B') {
-      Serial.println("delete");
-      Serial.print("A datacount=");
-      Serial.println(data_count);
-      data_count = data_count - 1;
-      if (data_count < 16) {
-        lcd.setCursor(data_count, 0);
-        lcd.print(" ");
-        Data[data_count] = '\0';
-
-      }
-      else {
-        lcd.setCursor(data_count - 16, 1);
-        lcd.print(" ");
-        Data[data_count] = '\0';
-      }
-    }
-    else if (customKey == 'A') {
-      send();
-      data_count = 0;
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("Massage Send...");
-      delay(1000);
-      lcd.clear();
-    }
-    else {
-      if (data_count < 32) {
-        Serial.println(customKey);
-        if (data_count < 16) {
-          lcd.setCursor(data_count, 0);
-          Data[data_count] = customKey;
-
-        }
-        else {
-          lcd.setCursor(data_count - 16, 1);
-          Data[data_count] = customKey;
-
-        }
-        lcd.print(customKey);
-        data_count++;
-        // delay(1000);
-        // lcd.clear();
-        // delay(80);
-      }
-      else {
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("Sorry massage ");
-        lcd.setCursor(0, 1);
-        lcd.print("too long..");
-        Serial.print("B & datacount=");
-        Serial.println(data_count);
-        int temp=data_count;//-------------
-        Data[data_count] = '\n';
-        //data_count--;
-       // Serial.print("B datacount=");
-        //Serial.println(data_count);
-        delay(1000);
-        lcd.clear();
-
-        for (int i = 0; i < 32; i++) {
-          if (i < 16) {
-            lcd.setCursor(i, 0);
-            lcd.print(Data[i]);
-          }
-          else {
-            lcd.setCursor(i - 16, 1);
-            lcd.print(Data[i]);
-          }
-          data_count=temp;//------
-        }
-      }
-
-    }
-  }
+				for (int i = 0; i < 32; i++) {
+					if (i < 16) {
+						lcd.setCursor(i, 0);
+						lcd.print(Data[i]);
+					}
+					else {
+						lcd.setCursor(i - 16, 1);
+						lcd.print(Data[i]);
+					}	
+					data_count=temp;//------
+				}
+			}
+		}
+	}
 }
+
 
 void send() {
   Serial.println(Data);
