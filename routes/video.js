af391d8b42a7fdf9df43120470d5514ea7a070de
Vihanga Paranahewa <31098256+VihangaParanahewa@users.noreply.github.com>
2018-05-29 15:45:02 +0530

    Implemented edit/delete function in videos

--- routes/video.js
+++ routes/video.js
@@ -61,6 +61,60 @@ router.post('/', function(req, res, next) {
 
 
 
+router.get('/edit', function(req, res, next) {
+    Video.find({_id: req.session.videoID}).exec(function (err, docs) {
+        assert.equal(null, err);
+        res.render('editVideo', {result : docs , csrfToken: req.csrfToken(), layout : 'main'});
+    });
+});
+
+router.get('/edit/:id', function(req, res, next) {
+    req.session.videoId = req.params.id;
+    res.redirect('/video/edit');
+});
+
+router.post('/edit', function(req, res, next) {
+
+    req.check('videoLink', 'cannot video Link empty ').notEmpty();
+    req.session.dat = new Date();
+    req.session.publishDate = req.session.dat.getFullYear() + "/" + (req.session.dat.getMonth()+1) + "/" +
+        req.session.dat.getDate() + "-" + req.session.dat.getHours() + ":" + req.session.dat.getMinutes() + ":"
+        + req.session.dat.getSeconds();
+
+
+    var errors = req.validationErrors();
+
+    if(errors){
+        req.session.errors = errors;
+        Video.find({_id: req.session.videoId}).exec(function (err, docs) {
+            assert.equal(null, err);
+            res.render('editVideo', {result : docs , messages : req.session.errors, layout : 'main'});
+        });
+    } else{
+        Video.findByIdAndUpdate(req.session.videoId, { $set: {
+
+            videoLink: req.body.videoLink,
+            ownerId: req.user._id,
+            eventId : req.session.eventID,
+            ownerName : req.user.firstName + " " + req.user.lastName,
+            ownerProfileImage : req.user.profileImage,
+            date : req.session.publishDate
+
+        }}, { new: true }, function (err, docs) {
+            assert.equal(null, err);
+            res.redirect('/video/publish');
+        });
+    }
+});
+
+router.get('/delete/:id', function(req, res, next) {
+    req.session.videoId = req.params.id;
+    Video.deleteOne({ _id : req.session.videoId }, function (err) {
+        assert.equal(null, err);
+        res.redirect('/video/publish');
+    });
+});
+
 
 
 function isLoggedIn(req, res, next) {
