279ad317c7c71959d849525ad36e7783c1adda52
Vihanga Paranahewa <31098256+VihangaParanahewa@users.noreply.github.com>
2018-08-21 01:19:07 +0530

    Optimize code in home page back-end

--- routes/index.js
+++ routes/index.js
@@ -16,45 +16,16 @@ var assert = require('assert');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
     Event.find().sort({_id : -1}).limit(4).exec(function (err, docs) {
-        req.session.eventChunks = [];
+        var eventChunks = [];
         var chunkSize = 2;
         for (var i = 0; i < docs.length; i += chunkSize) {
-            req.session.eventChunks.push(docs.slice(i, i + chunkSize));
+            eventChunks.push(docs.slice(i, i + chunkSize));
         }
-        //console.log(docs[0].title);
-        //console.log(docs[0]._id);
-        Map.find({eventId : docs[0]._id}).exec(function (err, result) {
-            if(result.length === 0){
-                res.render('index', {events: req.session.eventChunks});
-            }else {
-                req.session.regionBlock = [];
-                req.session.regionName = result[0].regionName;
-                req.session.matchingCount = 0;
-                for (var j = 0; j < result.length; j++) {
-                    if (req.session.regionName === result[j].regionName) {
-                        //console.log(req.session.regionName);
-                        req.session.matchingCount++;
-                        if (j === result.length - 1) {
-                            //console.log("I am In");
-                            req.session.regionBlock.push(result.slice((j - req.session.matchingCount + 1), j + 1));
-                            req.session.regionName = result[j].regionName;
-                            req.session.matchingCount = 0;
-                        }
-                    } else {
-                        req.session.regionBlock.push(result.slice((j - req.session.matchingCount), j));
-                        req.session.regionName = result[j].regionName;
-                        req.session.matchingCount = 0;
-                        j--;
-                    }
-                }
-                //console.log(req.session.eventChunks);
-                console.log(req.session.regionBlock[0][0].longitudes);
-                console.log(req.session.regionBlock[0].length);
-                res.render('index', {events: req.session.eventChunks, regionBlock: req.session.regionBlock});
-            }
-            });
+        res.render('index', {events: eventChunks});
     });
+
 });
 
 router.get('/newsFeed', function(req, res, next) {
@@ -113,8 +84,8 @@ router.get('/getJson', function(req, res, next) {
                 }
                 //console.log(req.session.eventChunks);
 
-                console.log(req.session.regionBlock[0][0].longitudes);
-                console.log(req.session.regionBlock[0].length);
+                //console.log(req.session.regionBlock[0][0].longitudes);
+                //console.log(req.session.regionBlock[0].length);
                 res.send(JSON.stringify(req.session.regionBlock));
             }
         });
