6ae6db0fbd8017543199e63e6774d89f363f672a
Vihanga Paranahewa <31098256+VihangaParanahewa@users.noreply.github.com>
2018-08-22 13:41:37 +0530

    Implemented the back-end of the device message box

--- routes/chatBox.js
+++ routes/chatBox.js
@@ -3,7 +3,9 @@ var router = express.Router();
 var csrf = require('csurf');
 var csrfProtection = csrf();
 var passport = require('passport');
-var messageFromDevices=require('../models/messageFromDevice');
+
+var messageFromDevice=require('../models/messageFromDevice');
+var Event = require('../models/event');
 
 router.use(csrfProtection);
 
@@ -13,21 +15,23 @@ router.get('/msg/:msg/:address', function(req, res, next) {
     var dateTime = require('node-datetime');
     var dt = dateTime.create();
     var formatted = dt.format('Y-m-d H:M:S');
-    var massage = new messageFromDevices();
-    massage.deviceAddress = address;
-    massage.massage =massage ;
-    massage.time =formatted ;
-    massage.save(function (err, result) {
-        if(err){
-            res.send(err);
-        }
-        else{
-            res.send("Massage Reseved:"+msg+" Sender Address :"+address+" Time :"+formatted);
-        }
+    Event.find().sort({_id : -1}).limit(1).exec(function (err, docs) {
+        var massage = new messageFromDevice();
+        message.eventId = docs[0]._id;
+        massage.deviceAddress = address;
+        massage.massage = massage;
+        massage.time = formatted;
+        massage.save(function (err, result) {
+            if (err) {
+                res.send(err);
+            }
+            else {
+                res.send("Massage Reseved:" + msg + " Sender Address :" + address + " Time :" + formatted);
+            }
+        });
     });
-
-
 });
+
 
 
 router.use('/', isLoggedIn, function(req, res, next) {
@@ -39,7 +43,17 @@ router.get('/', function(req, res, next) {
     res.render('chatBox',{layout : 'main'});
 });
 
+router.get('/deviceMessageBox', function(req, res, next) {
+    messageFromDevice.find({eventId : req.session.eventID}).exec(function (err, docs) {
+        var messageChunks = [];
+        var chunkSize = 2;
+        for (var i = 0; i < docs.length; i += chunkSize) {
+            messageChunks.push(docs.slice(i, i + chunkSize));
+        }
+        res.render('deviceMessageBox', {messages: messageChunks, layout: 'main'});
+        });
 
+});
 
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated() && (req.user.role === 'Admin' || req.user.role === 'Super Volunteer') ){
